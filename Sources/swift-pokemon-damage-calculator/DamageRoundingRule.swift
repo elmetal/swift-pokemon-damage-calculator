e294ab81d6f88325baa70e7242a61f091fa8c34b
elmetal <nm7.pupu@gmail.com>
2026-04-25 04:34:40 +0900

    add DamageRoundingRule

---
+++ Sources/swift-pokemon-damage-calculator/DamageRoundingRule.swift
@@ -0,0 +1,12 @@
+//
+//  DamageRoundingRule.swift
+//  swift-pokemon-damage-calculator
+//  
+//  Created by elmetal on 2026/04/25
+//  
+//
+
+enum DamageRoundingRule: Equatable {
+    case down
+    case toNearestOrDown
+}
